b25ae5b2b6f99be1b87b471080ab929bc9e31409
juniocezar <juniocezar.dcc@gmail.com>
2018-07-21 16:34:16 -0300

    fixing signing scr

--- sign.sh
+++ sign.sh
@@ -3,6 +3,9 @@
 echo "Signing $1 file"
 
 mkdir -p install _tmp
+rm -r install _tmp
+mkdir -p install _tmp
+
 app=$( basename $1 )
 
 cp $1 _tmp/
@@ -10,18 +13,18 @@ cd _tmp
 
 apktool d $app
 
-cd ${app::-4}
-awk 'NR==1{print; print "    <uses-permission android:name=\"android.permission.WRITE_EXTERNAL_STORAGE\" />"} NR!=1' AndroidManifest.xml > tmp.xml
-mv tmp.xml AndroidManifest.xml
-cd ..
+#cd ${app::-4}
+#awk 'NR==1{print; print "    <uses-permission android:name=\"android.permission.WRITE_EXTERNAL_STORAGE\" />"} NR!=1' AndroidManifest.xml > tmp.xml
+#mv tmp.xml AndroidManifest.xml
+#cd ..
 
 apktool b ${app::-4} -o toSign-${app}
 
 
 
-keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore toSign-${app} alias_name
+#keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 zipalign -v 4 toSign-${app} ../install/$app
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../key/debug.keystore ../install/$app alias_name
 
 cd ..
 rm -r _tmp/
